08068ae39c7fb956d1ff80759c517fec4334481a
Hakan <s.hakanayata@gmail.com>
2023-06-17 21:43:25 +0200

    removed event listener

--- journal/static/journal/js/getEntries.js
+++ journal/static/journal/js/getEntries.js
@@ -1,19 +1,15 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const originURL = window.location.origin
-    let data;
+const originURL = window.location.origin
+let data;
 
-    async function getEntries() {
-        if (data) return data
-        try {
-            const res = await fetch(`${originURL}/entries`)
-            if (!res.ok) { throw new Error("Could not fetch data!") }
-            data = await res.json()
-            console.log(data);
-            return data
-        } catch (error) {
-            console.error(error)
-        }
+async function getEntries() {
+    if (data) return data
+    try {
+        const res = await fetch(`${originURL}/entries`)
+        if (!res.ok) { throw new Error("Could not fetch data!") }
+        data = await res.json()
+        // console.log("getEntries: ", data);
+        return data
+    } catch (error) {
+        console.error(error)
     }
-
-    getEntries()
-})
+}
