005f2a0fd3133d4415b4bbcd84b550db03666f5c
Hakan <s.hakanayata@gmail.com>
2023-06-17 12:07:06 +0200

    fix days in last row

--- journal/static/journal/js/board.js
+++ journal/static/journal/js/board.js
@@ -2,8 +2,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const boardHead = document.getElementById("boardHead")
     const activityBoard = document.getElementById("activityBoard")
     let numberOfDays;
+    let currentDate = new Date()
     const getDaysOfCurMonth = () => {
-        const currentDate = new Date()
         const currentYear = currentDate.getFullYear()
         const currentMonth = currentDate.getMonth() + 1
 
@@ -13,11 +13,11 @@ document.addEventListener('DOMContentLoaded', () => {
     }
     numberOfDays = getDaysOfCurMonth()
     // if Thursday, getDay() returns 4, it's one-based instead of zero
-    const startsAtNthDay = new Date(new Date().setDate(1)).getDay()
-    // get zero-based index of starting day of this month
+    const startsAtNthDay = new Date(currentDate.setDate(1)).getDay()
+    // get zero-based index (0 - 6) of starting day of this month
     const indexOfStartAtNthDay = startsAtNthDay - 1
     numberOfDays += indexOfStartAtNthDay
-    const daysInLastWeek = numberOfDays % 7
+    const daysInLastWeek = numberOfDays % 7 === 0 ? 7 : numberOfDays % 7
     const numberOfWeeks = Math.ceil(numberOfDays / 7)
     // console.log(numberOfDays, numberOfWeeks, daysInLastWeek);
 
@@ -33,7 +33,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 newRow.appendChild(newCell)
             }
             boardBody.appendChild(newRow)
-        } else if (i === 4) {
+        } else if (i === numberOfWeeks - 1) {
             newRow.className = "d-flex align-items-center justify-content-start gap-2"
             for (let k = 0; k < daysInLastWeek; k++) {
                 const newLastWeeksCell = document.createElement("td")
