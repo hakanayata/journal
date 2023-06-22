05fa0d14ce61c21f7d83cffce5dfe18a6c145fbb
Hakan <s.hakanayata@gmail.com>
2023-06-22 12:08:41 +0200

    profile page create

--- journal/urls.py
+++ journal/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path("create_entry/<str:date_str>", views.create_entry, name="create_entry"),
     path("update_entry/<str:date_str>", views.update_entry, name="update_entry"),
     path("all_entries", views.all_entries, name="all_entries"),
+    path("profile", views.profile, name="profile"),
     # API Routes
     path("entries", views.entries, name="entries"),
     path("entry_on/<str:date>", views.entry_on, name="entry_on"),
