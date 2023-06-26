f37a6be52bcae1ca0eca854341bfe9ceb51d1082
Hakan <s.hakanayata@gmail.com>
2023-06-26 17:22:07 +0200

    fix: after del highlight stays

--- journal/static/journal/js/script.js
+++ journal/static/journal/js/script.js
@@ -1,8 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
+    let calendarDayCells = document.querySelectorAll("td")
+    const xd = document.getElementsByTagName("td")
 
-    // Redirect user to new entry page on button click
     document.addEventListener("click", (event) => {
         const element = event.target
+        // Redirect user to new entry page on button click
         if (element.id === "newEntryBtn") {
             const daysDateEl = document.getElementById("daysInvisDate")
             location.href = `${originURL}/create_entry/${daysDateEl.value}`
@@ -19,11 +21,20 @@ document.addEventListener("DOMContentLoaded", () => {
                     throw new Error("Oops! Something went wrong.")
                 }
                 displayAlert("success", "Entry deleted.")
-                // todo: remove entry from DOM // get date from data-date
+
+                // Display entry view of deleted day
                 getEntry(dateToDelete).then(data => displayDaysEntry(data, dateToDelete))
 
+                // Remove highlighting
+                calendarDayCells.forEach(boardCell => {
+                    if (boardCell.dataset.date === dateToDelete) {
+                        boardCell.style.backgroundColor = "#eee"
+                    }
+                })
             }).catch(err => displayAlert("error", err))
+
         } else if (element.dataset.date) {
+            // Show day's entry that is being clicked
             const clickedDate = element.dataset.date
             getEntry(clickedDate).then(data => displayDaysEntry(data, clickedDate))
         }
